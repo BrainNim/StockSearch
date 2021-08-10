2a1922a088ada258ba01b79601d2d5bffa87177b
Lee <hhhsss0815@gmail.com>
2021-08-11 01:35:42 +0900

    파라미터 키 중복으로인한 누락 문제 해결

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pymysql
 import pandas as pd
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from search_filter import *
 
 # connect mysql
@@ -14,37 +14,25 @@ app = Flask(__name__)
 
 # http://127.0.0.1:5000/?MarketFilter.market=KOSPI
 # http://127.0.0.1:5000/?MarketFilter.market=KOSPI&PriceFilter.compare_max=0.7
+# http://127.0.0.1:5000/?MarketFilter.market=KOSPI&PriceFilter.compare_max=0.7&MarketFilter.market=KOSDAQ
 # http://127.0.0.1:5000/?MarketFilter.market=KOSPI&PriceFilter.updown=1000,10000&PriceFilter.compare_max=0.7
 @app.route('/')
 def filter():
-
     df = pd.read_sql("select * from stocksearch.daily_market", conn)
 
-    parameter_dict = request.args.to_dict()
-    if len(parameter_dict) == 0:
+    query = request.query_string.decode('utf-8')
+    parameters = query.split('&')
+
+    if len(parameters) == 0:
         return df.ID.to_string()
 
-    # http://127.0.0.1:5000/?MarketFilter=market:KOSPI&PriceFilter=updown:1000,10000&PriceFilter=compare_max:0.7
-    # for key in parameter_dict.keys():
-    #     func = key
-    #     method = request.args[key].split(':')[0]
-    #     values = request.args[key].split(':')[-1].split(',')
+    for param in parameters:
+        func = param.split('=')[0].split('.')[0]
+        method = param.split('=')[0].split('.')[-1]
+        values = param.split('=')[-1]
+        filter_func = getattr(globals()[func](conn), method)
+        df = filter_func(values, df)
 
-    s = ""
-    for key in parameter_dict.keys():
-        func = key.split('.')[0]
-        method = key.split('.')[-1]
-
-        values = request.args[key].split(',')
-        s += f"{func} / {method} / {values} \n"
-
-        filter = getattr(globals()[func](conn), method)
-        print()
-        print(values[0], type(values[0]))
-        print()
-        df = filter(values[0], df)
-
-    # return s
     return str(df.ID.to_list())
 
 if __name__ == "__main__":

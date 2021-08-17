93f30f8f27e189a4ec099e41118de95fc1a2093f
Lee <hhhsss0815@gmail.com>
2021-08-17 11:19:54 +0900

    foldering

---
+++ backend/daily_update.py
@@ -0,0 +1,118 @@
+from pykiwoom.kiwoom import *
+import datetime
+import pymysql
+import pandas as pd
+import time
+
+start_num = 0
+
+# today
+now = datetime.datetime.now()
+today = now.strftime("%Y%m%d")
+print(today)
+
+# Login
+kiwoom = Kiwoom()
+kiwoom.CommConnect(block=True)
+print('로그인 성공')
+
+# connect mysql
+conn = pymysql.connect(host="localhost",
+                       user="root",
+                       password="0000",
+                       db="stocksearch")
+curs = conn.cursor()
+yesterday_df = pd.read_sql("select * from stocksearch.daily_market", conn)
+
+
+# get_code_list_by_market (펀드, etf제거)
+kospi_code = kiwoom.GetCodeListByMarket('0')
+kosdaq_code = kiwoom.GetCodeListByMarket('10')
+mutual_code = kiwoom.GetCodeListByMarket('4')
+etf_code = kiwoom.GetCodeListByMarket('8')
+
+total_code = (set(kosdaq_code) | set(kospi_code)) - (set(mutual_code) | set(etf_code))
+total_code = list(total_code)
+
+market_code = []
+for code in total_code:
+    if code in kospi_code:
+        market_code.append('KOSPI')
+    elif code in kosdaq_code:
+        market_code.append('KOSDAQ')
+
+print("총 종목 수 :", len(total_code), '(ETN 종목 포함)')
+
+
+# 기존 종목 리스트와 다른 점이 있다면 종목 추가/수정
+if len(total_code) != len(yesterday_df):
+    new_code = list(set(total_code) - set(yesterday_df['ID']))
+    del_code = list(set(yesterday_df['ID']) - set(total_code))
+
+    if len(new_code) > 0:
+        for code in new_code:
+            curs.execute(f"INSERT INTO stocksearch.daily_market (ID) VALUES ('{code}')")
+        conn.commit()
+        print(len(new_code), '개 신규종목 추가')
+
+    if len(del_code) > 0:  # 추후 데이터에서 제거하는 것이 아닌 IS_ENABLE을 추가, 변경
+        for code in del_code:
+            curs.execute(f"DELETE FROM stocksearch.daily_market WHERE ID = '{code}'")
+        conn.commit()
+        print(len(del_code), '개 폐지종목 제거')
+
+
+# 종목별 오늘 시황 업데이트
+daily_df = pd.read_sql("select * from stocksearch.daily_market", conn)
+code_li = daily_df['ID']
+
+print('조회제한 방지 위해 10초 휴식중...')
+time.sleep(10)
+for i in range(start_num, len(code_li)):
+    code = code_li[i]
+    print(f"{code} ({i}/{len(code_li)})")
+    time.sleep(0.6)
+    today_df = kiwoom.block_request("opt10001",
+                                    종목코드=code,
+                                    기준일자=today,
+                                    수정주가구분=1,
+                                    output="주식기본정보요청",
+                                    next=0)
+    today_df = today_df.iloc[0]
+    today_df[today_df.isin([""])] = 'NULL'  # 결측치 처리
+
+    # ETN 종목 제거
+    if 'ETN' in today_df['종목명']:
+        curs.execute(f"DELETE FROM stocksearch.daily_market WHERE ID = '{code}'")
+        conn.commit()
+        print(f"{code} ({i}/{len(code_li)}) 제거 - 사유: ETN 종목")
+
+        continue
+
+    # daily_market 업데이트
+    update_sql_1 = f"""UPDATE stocksearch.daily_market
+                    SET Name = "{today_df['종목명']}", Market = "{market_code[i]}", Capital = {today_df['시가총액']},
+                    PER = {today_df['PER']}, EPS = {today_df['EPS']}, ROE = {today_df['ROE']},
+                    PBR = {today_df['PBR']}, EV = {today_df['EV']}, BPS = {today_df['BPS']},
+                    Revenue = {today_df['매출액']}, Operating_Income = {today_df['영업이익']}, Net_Income = {today_df['당기순이익']},
+                    Open = {abs(int(today_df['시가']))}, High = {abs(int(today_df['고가']))}, Low = {abs(int(today_df['저가']))},
+                    Close = {abs(int(today_df['현재가']))}, Volume = {abs(int(today_df['거래량']))},
+                    Highest_Price = {abs(int(today_df['250최고']))}, Highest_Date = {today_df['250최고가일']}
+                    WHERE ID = '{code}'; """
+    curs.execute(update_sql_1)
+    conn.commit()
+
+    # past_market 업데이트
+    update_sql_2 = f"""INSERT INTO stocksearch.past_market
+            (ID, Date, Open, High, Low, Close, Volume) Values
+            ('{code}', {today}, {abs(int(today_df['시가']))}, {abs(int(today_df['고가']))},
+            {abs(int(today_df['저가']))}, {abs(int(today_df['현재가']))}, {abs(int(today_df['거래량']))}); """
+    curs.execute(update_sql_2)
+    conn.commit()
+
+    # if i % 1000 == 999:  # 1000개마다 60초 휴식
+    #     print('조회제한 방지 위해 60초 휴식 후 재로그인')
+    #     kiwoom.CommConnect(block=True)
+    #     time.sleep(60)
+
+conn.close()
---
+++ backend/search_filter.py
@@ -0,0 +1,201 @@
+import pandas as pd
+import datetime
+
+
+class MarketFilter:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def market(self, market, df):
+        new_df = df[df['Market'] == market]
+        return new_df
+
+    def category(self, category, df):
+        new_df = df[df['Market'] == category]
+        return new_df
+
+
+class PriceFilter:
+    def __init__(self, conn):
+        self.conn = conn
+        schedule = pd.read_sql(f"""SELECT Date FROM stocksearch.past_market GROUP BY Date ORDER BY Date DESC;""", conn)
+        self.recent_date = schedule.iloc[0].values[0]  # 최근 시장오픈
+
+    def updown(self, down, up, df):
+        down, up = float(down), float(up)
+        new_df = df[(df['Close'] <= up) & (df['Close'] >= down)]
+        return new_df
+
+    def compare_mean(self, day, times, updown, df):  # 기간평균가와 현재가 비교
+        day, times = int(day), float(times)
+        code_tuple = tuple(df['ID'])
+        mean_df = pd.read_sql(f"""SELECT ID, AVG(Close) as Mean  FROM stocksearch.past_market 
+                            WHERE Date >= Date(subdate("{self.recent_date}", INTERVAL {day} DAY))
+                            GROUP BY ID;""", self.conn)
+        new_df = pd.merge(mean_df, df, left_on='ID', right_on='ID', how='inner')
+        if updown == 'up':
+            new_df = new_df[new_df['Close'] >= new_df['Mean'] * times]
+        else:
+            new_df = new_df[new_df['Close'] <= new_df['Mean'] * times]
+        new_df = new_df.drop('Mean', axis=1)
+        return new_df
+
+    def compare_max(self, times, df):  # 250일 최고가와 현재가 비교
+        times = float(times)
+        new_df = df[df['Close'] <= df['Highest_Price'] * times]
+        return new_df
+
+    def dist_max(self, day, inout, df):  # 250일 최고가일이 오늘과 몇일 떨어져있나
+        day = int(day)
+        cont_day = datetime.datetime.now() - datetime.timedelta(days=day)
+        if inout == 'in':
+            new_df = df[df['Highest_Date'] >= cont_day.date()]
+        else:
+            new_df = df[df['Highest_Date'] <= cont_day.date()]
+        return new_df
+
+
+class VolumeFilter:
+    def __init__(self, conn):
+        self.conn = conn
+        schedule = pd.read_sql(f"""SELECT Date FROM stocksearch.past_market GROUP BY Date ORDER BY Date DESC;""", conn)
+        self.recent_date = schedule.iloc[0].values[0]  # 최근 시장오픈
+
+    def updown(self, down, up, df):
+        down, up = float(down), float(up)
+        new_df = df[(df['Volume'] <= up) & (df['Volume'] >= down)]
+        return new_df
+
+    def compare_mean(self, day, times, updown, df):
+        day, times = int(day), float(times)
+        mean_df = pd.read_sql(f"""SELECT ID, AVG(Volume) as Mean FROM stocksearch.past_market 
+                            WHERE Date >= Date(subdate("{self.recent_date}", INTERVAL {day} DAY))
+                            GROUP BY ID;""", self.conn)
+        new_df = pd.merge(mean_df, df, left_on='ID', right_on='ID', how='inner')
+        if updown == 'up':
+            new_df = new_df[new_df['Volume'] >= new_df['Mean'] * times]
+        else:
+            new_df = new_df[new_df['Volume'] <= new_df['Mean'] * times]
+        new_df = new_df.drop('Mean', axis=1)
+        return new_df
+
+    # def compare_max(self, times, df, conn):
+    #     for code in df['ID']:
+    #         new_df = pd.read_sql(f"""SELECT ID, MAX(Volume) as Max FROM stocksearch.past_market
+    #                     WHERE ID = '{code}' """, self.conn)
+
+
+class PBRFilter:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def updown(self, down, up, df):
+        down, up = float(down), float(up)
+        new_df = df[(df['PBR'] <= up) & (df['PBR'] >= down)]
+        return new_df
+
+    def top(self, topdown, n, df):
+        n = int(n)
+        if topdown == 'top':
+            new_df = df.sort_values(by='PBR', ascending=False).head(n)
+        else:
+            new_df = df.sort_values(by='PBR', ascending=True).head(n)
+        return new_df
+
+
+class PERFilter:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def updown(self, down, up, df):
+        down, up = float(down), float(up)
+        new_df = df[(df['PER'] <= up) & (df['PER'] >= down)]
+        return new_df
+
+    def top(self, topdown, n, df):
+        n = int(n)
+        if topdown == 'top':
+            new_df = df.sort_values(by='PER', ascending=False).head(n)
+        else:
+            new_df = df.sort_values(by='PER', ascending=True).head(n)
+        return new_df
+
+
+class ROAFilter:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def updown(self, down, up, df):
+        down, up = float(down), float(up)
+        new_df = df[(df['ROA'] <= up) & (df['ROA'] >= down)]
+        return new_df
+
+    def top(self, topdown, n, df):
+        n = int(n)
+        if topdown == 'top':
+            new_df = df.sort_values(by='ROA', ascending=False).head(n)
+        else:
+            new_df = df.sort_values(by='ROA', ascending=True).head(n)
+        return new_df
+
+
+class ROEFilter:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def updown(self, down, up, df):
+        down, up = float(down), float(up)
+        new_df = df[(df['ROE'] <= up) & (df['ROE'] >= down)]
+        return new_df
+
+    def top(self, topdown, n, df):
+        n = int(n)
+        if topdown == 'top':
+            new_df = df.sort_values(by='ROE', ascending=False).head(n)
+        else:
+            new_df = df.sort_values(by='ROE', ascending=True).head(n)
+        return new_df
+
+
+class CrossFilter:
+    def __init__(self, conn):
+        self.conn = conn
+        schedule = pd.read_sql(f"""SELECT Date FROM stocksearch.past_market GROUP BY Date ORDER BY Date DESC;""", conn)
+        self.recent_date = schedule.iloc[0].values[0]  # 최근 시장오픈
+
+    def get_cross_sql(self, short, long):
+        short, long = int(short), int(long)
+        cross_sql = f"""select TS.ID, TS, TL, YS, YL from 
+                            (SELECT ID, AVG(Close) as TS FROM stocksearch.past_market 
+                                                        WHERE Date >= Date(subdate("{self.recent_date}", INTERVAL {short} DAY))
+                                                        GROUP BY ID) as TS
+                            join
+                            (SELECT ID, AVG(Close) as TL FROM stocksearch.past_market 
+                                                        WHERE Date >= Date(subdate("{self.recent_date}", INTERVAL {long} DAY))
+                                                        GROUP BY ID) as TL on TS.ID = TL.ID
+                            join
+                            (SELECT ID, AVG(Close) as YS FROM stocksearch.past_market 
+                                                        WHERE Date >= Date(subdate("{self.recent_date}", INTERVAL {short}+1 DAY)) and Date < Date(subdate("{self.recent_date}", INTERVAL 1 DAY))
+                                                        GROUP BY ID) as YS on TS.ID = YS.ID
+                            join
+                            (SELECT ID, AVG(Close) as YL FROM stocksearch.past_market 
+                                                        WHERE Date >= Date(subdate("{self.recent_date}", INTERVAL {long}+1 DAY)) and Date < Date(subdate("{self.recent_date}", INTERVAL 1 DAY))
+                                                        GROUP BY ID) as YL on TS.ID = YL.ID ;"""
+        return cross_sql
+
+    def goldencross(self, short, long, df):
+        goldencross_sql = self.get_cross_sql(short, long)
+        mean_df = pd.read_sql(goldencross_sql, self.conn)
+        mean_df = mean_df[(mean_df['TS'] > mean_df['TL']) & (mean_df['YS'] < mean_df['YL'])]
+        new_df = pd.merge(mean_df, df, left_on='ID', right_on='ID', how='inner')
+        new_df = new_df.drop(['TS', 'TL', 'YS', 'YL'], axis=1)
+        return new_df
+
+    def deadcross(self, short, long, df):
+        deadcross_sql = self.get_cross_sql(short, long)
+        mean_df = pd.read_sql(deadcross_sql, self.conn)
+        mean_df = mean_df[(mean_df['TS'] < mean_df['TL']) & (mean_df['YS'] > mean_df['YL'])]
+        new_df = pd.merge(mean_df, df, left_on='ID', right_on='ID', how='inner')
+        new_df = new_df.drop(['TS', 'TL', 'YS', 'YL'], axis=1)
+        return new_df
+
